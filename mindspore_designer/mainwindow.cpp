bfcf0160da330b38b883c24e492e01976d5c8099
pengliu <connect_liupeng@163.com>
2024-01-05 15:52:51 +0800

    添加框架界面及工具栏图标

--- mindspore_designer/mainwindow.cpp
+++ mindspore_designer/mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->menu_data->menuAction()->setVisible(false);
+    ui->menu_designer->menuAction()->setVisible(true);
+    ui->menu_train->menuAction()->setVisible(false);
+    ui->toolBar_data->hide();
+    ui->toolBar_designer->show();
+    ui->toolBar_train->hide();
+
+    ui->widget->show();
+    ui->widget_2->show();
+    ui->widget_3->show();
 
 }
 
@@ -17,9 +30,51 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_tabWidget_tabBarClicked(int index)
 {
-//     ui->menu_data->hide();
-//     ui->menu_2->hide();
-     ui->menu_data->hideTearOffMenu();
-     ui->menu_2->hideTearOffMenu();
+    if(0 == index)
+    {
+        qDebug()<<"pag 设计器";
+//        QMenu::menuAction()->setVisible(false)
+        ui->menu_data->menuAction()->setVisible(false);
+        ui->menu_designer->menuAction()->setVisible(true);
+        ui->menu_train->menuAction()->setVisible(false);
+        ui->toolBar_data->hide();
+        ui->toolBar_designer->show();
+        ui->toolBar_train->hide();
+
+        ui->widget->show();
+        ui->widget_2->show();
+        ui->widget_3->show();
+    }
+    else if(1 == index)
+    {
+        qDebug()<<"page 数据";
+        ui->menu_data->menuAction()->setVisible(true);
+        ui->menu_designer->menuAction()->setVisible(false);
+        ui->menu_train->menuAction()->setVisible(false);
+        ui->toolBar_data->show();
+        ui->toolBar_designer->hide();
+        ui->toolBar_train->hide();
+
+        ui->widget->hide();
+        ui->widget_2->hide();
+        ui->widget_3->hide();
+
+    }
+    else if(2 == index)
+    {
+        qDebug()<<"pag 训练";
+        ui->menu_data->menuAction()->setVisible(false);
+        ui->menu_designer->menuAction()->setVisible(false);
+        ui->menu_train->menuAction()->setVisible(true);
+        ui->toolBar_data->hide();
+        ui->toolBar_designer->hide();
+        ui->toolBar_train->show();
+
+        ui->widget->hide();
+        ui->widget_2->hide();
+        ui->widget_3->hide();
+    }
+
+
 }
 
